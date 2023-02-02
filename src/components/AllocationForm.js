c41213fab75517a9466b74a4e0e33cf2e6171026
Oyetechn <113299215+Oyetechn@users.noreply.github.com>
2023-02-02 16:32:55 +0200

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,1 +1,92 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const AllocationForm = (props) => {
+    const { dispatch, remaining, currency } = useContext(AppContext);
+
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+    const [action, setAction] = useState('');
+
+    const submitEvent = () => {
+        if(cost > remaining) {
+            alert("The value cannot exceed remaining funds "+currency+remaining);
+            setCost("");
+            return;
+        }
+        else if (isNaN(parseInt(cost))) {
+        //or use: else if(cost === "") {
+            alert("This field only accepts numbers.");
+            // Note: setCost("") will not change the state because the input field parses anything that is not a number as an empty string, so I added a whitespace to setCost(" ");
+            setCost(" ");
+            return;
+        }
+
+        const expense = {
+            name: name,
+            cost: parseInt(cost),
+        };
+        if(action === "Reduce") {
+            dispatch({
+                type: 'RED_EXPENSE',
+                payload: expense,
+            });
+        } else {
+                dispatch({
+                    type: 'ADD_EXPENSE',
+                    payload: expense,
+                });
+            }
+    };
+
+    return (
+        <div>
+            <div className='row'>
+
+            <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                    <div className="input-group-prepend">
+                <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
+                </div>
+                  <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                        <option defaultValue>Choose...</option> 
+                        <option value="Marketing" name="marketing"> Marketing</option>
+                <option value="Sales" name="sales">Sales</option>
+                <option value="Finance" name="finance">Finance</option>
+                <option value="HR" name="hr">HR</option>
+                <option value="IT" name="it">IT</option>
+                <option value="Admin" name="admin">Admin</option>
+                  </select>
+
+                    <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+                </div>
+                  <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                        <option defaultValue value="Add" name="Add">Add</option>
+                <option value="Reduce" name="Reduce">Reduce</option>
+                  </select>
+
+                    <div className='input-group-prepend' style={{marginLeft: '2rem' }}>
+                        <label htmlFor='inputGroupSelect03'>
+                            {currency}
+                        </label>
+                    </div>
+                    <input
+                        required='required'
+                        type='number'
+                        id='inputGroupSelect03'
+                        value={cost}
+                        style={{ marginLeft: '0.5rem' , size: 10}}
+                        onChange={(event) => setCost(event.target.value)}>
+                        </input>
+
+                    <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                        Save
+                    </button>
+                </div>
+                </div>
+
+        </div>
+    );
+};
+
+export default AllocationForm;
